GrapheneOS-Archive/platform_frameworks_base-old

default to GrapheneOS connectivity check server

services/core/java/com/android/server/ConnectivityService.java
@@ -275,7 +275,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      * (preferably via runtime resource overlays).
      */
     private static final String DEFAULT_CAPTIVE_PORTAL_HTTP_URL =
-            "http://connectivitycheck.gstatic.com/generate_204";
+            "http://connectivitycheck.grapheneos.org/generate_204";
 
     // TODO: create better separation between radio types and network types
 

MaximilianGaedig/platform_frameworks_base

Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

services/core/java/com/android/server/ConnectivityService.java
@@ -278,7 +278,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      * (preferably via runtime resource overlays).
      */
     private static final String DEFAULT_CAPTIVE_PORTAL_HTTP_URL =
-            "http://connectivitycheck.gstatic.com/generate_204";
+            "http://connectivitycheck.grapheneos.network/generate_204";
 
     // TODO: create better separation between radio types and network types
 